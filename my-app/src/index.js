a1640c3a314fe69e2bdba8c9c0d9d5b7df6a8206
WePung <117412075+WePung@users.noreply.github.com>
2023-04-06 12:43:48 +0900

    2023년 4월 6일 강의 정리(미완료)

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+// import App from './App';
 import reportWebVitals from './reportWebVitals';
-import Clock from './clock/Clock';
+import CommentList from './chapter_05/CommentList';
+import NotificationList from './chapter_06/NotificationList';
 
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Clock />
+    <NotificationList />
   </React.StrictMode>
 );
 
