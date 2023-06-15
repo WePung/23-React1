7ff48cea7037cfb10c638dd5c6eeeae43af75dd4
WePung <117412075+WePung@users.noreply.github.com>
2023-06-15 13:36:05 +0900

    2023년 6월 15일 1학기 기말고사 실기 두번째 커밋
    정상작동 확인

--- final/src/App.js
+++ final/src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  // 색을 지정하기위해 사용 - 초기값은 Purple
+  const [color, setColor] = useState("Purple");
+  // 입력이 들어 올 때마다 입력한 값을 감지하여 색을 변경하기 위한 기능
+  const onChangeColor = (e) => {
+    setColor(e.target.value);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    // 입력 받을 때 마다 color가 변경되어 반영됨
+    <div className="App" style={{ background: `${color}` }}>
+      <div className="wrapper">
+        <label>Background Color : </label>
+        <input
+          onChange={(e) => {
+            onChangeColor(e);
+          }}
+          value={color}
+        />
+      </div>
     </div>
   );
 }
